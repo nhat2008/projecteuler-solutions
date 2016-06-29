1fd1b3d60ac3b58112a836b6331bd2366bbc873d
Nhat Nguyen <nhatnguyen@Nhats-MacBook-Pro.local>
2016-06-30 00:17:44 +0700

    Problem 7

--- pro_007_10001st_prime.py
+++ pro_007_10001st_prime.py
@@ -2,10 +2,6 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #
 # What is the 10 001st prime number?
-
-import math
-
-
 def get_prime(n):
     x = 3
     while n % 2 == 0 and n != 2:
